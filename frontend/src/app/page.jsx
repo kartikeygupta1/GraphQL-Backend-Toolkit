5ff36df04259c181aa0363d01327a7357607e847
kartikeygupta1 <kartikraja522@gmail.com>
2024-03-23 16:00:46 +0530

    hero

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -1,58 +1,231 @@
+'use client';
 import React from 'react';
 import { Image, Container, Title, Button, Group, Text, ThemeIcon, rem, Box } from '@mantine/core';
 import { IconCheck } from '@tabler/icons-react';
 // import { GithubIcon } from '@mantinex/dev-icons';
 import classes from './page.module.css';
 import { Icon3dRotate } from '@tabler/icons-react';
+// Feature Section Imports
+import { SimpleGrid, Grid } from '@mantine/core';
+import { IconReceiptOff, IconFlame, IconCircleDotted, IconFileCode } from '@tabler/icons-react';
+
+//Testimonials Imports
+import { Carousel } from '@mantine/carousel';
+import { useMediaQuery } from '@mantine/hooks';
+import { Paper, useMantineTheme } from '@mantine/core';
+
+const features = [
+  {
+    icon: IconReceiptOff,
+    title: 'Free and open source',
+    description: 'All packages are published under MIT license, you can use Mantine in any project',
+  },
+  {
+    icon: IconFileCode,
+    title: 'TypeScript based',
+    description: 'Build type safe applications, all components and hooks export types',
+  },
+  {
+    icon: IconCircleDotted,
+    title: 'No annoying focus ring',
+    description:
+      'With new :focus-visible selector focus ring will appear only when user navigates with keyboard',
+  },
+  {
+    icon: IconFlame,
+    title: 'Flexible',
+    description:
+      'Customize colors, spacing, shadows, fonts and many other settings with global theme object',
+  },
+];
 
 export default function Home() {
+
+  const items = features.map((feature) => (
+    <div key={feature.title}>
+      <ThemeIcon
+        size={44}
+        radius="md"
+        variant="gradient"
+        gradient={{ deg: 133, from: 'blue', to: 'cyan' }}
+      >
+        <feature.icon style={{ width: rem(26), height: rem(26) }} stroke={1.5} />
+      </ThemeIcon>
+      <Text fz="lg" mt="sm" fw={500}>
+        {feature.title}
+      </Text>
+      <Text c="dimmed" fz="sm">
+        {feature.description}
+      </Text>
+    </div>
+  ));
+
   return (
-    <Container size="md">
-      <div className={classes.inner}>
-        <div className={classes.content}>
-          <Title className={classes.title}>
-            A <span className={classes.highlight}>modern</span> React <br /> components library
-          </Title>
-          <Text c="dimmed" mt="md">
-            Build fully functional accessible web applications faster than ever – Mantine includes
-            more than 120 customizable components and hooks to cover you in any situation
-          </Text>
+    <>
+      <Container size="md">
+        <div className={classes.inner}>
+          <div className={classes.content}>
+            <Title className={classes.title}>
+              Graph <span className={classes.highlight}>QL</span> Querry Language <br /> Backend Code Generator
+            </Title>
+            <Text c="dimmed" mt="md">
+              Build fully functional accessible web applications faster than ever – Mantine includes
+              more than 120 customizable components and hooks to cover you in any situation
+            </Text>
 
-          <Group
-            mt={30}
-            spacing="sm"
-            size="sm"
-            icon={
-              <ThemeIcon size={20} radius="xl">
-                <IconCheck style={{ width: rem(12), height: rem(12) }} stroke={1.5} />
-              </ThemeIcon>
-            }
-          >
-            <Box>
-              <b>TypeScript based</b> – build type safe applications, all components and hooks
-              export types
-            </Box>
-            <Box>
-              <b>Free and open source</b> – all packages have MIT license, you can use Mantine in
-              any project
-            </Box>
-            <Box>
-              <b>No annoying focus ring</b> – focus ring will appear only when user navigates with
-              keyboard
-            </Box>
-          </Group>
+            <Group
+              mt={30}
+              spacing="sm"
+              size="sm"
+              icon={
+                <ThemeIcon size={20} radius="xl">
+                  <IconCheck style={{ width: rem(12), height: rem(12) }} stroke={1.5} />
+                </ThemeIcon>
+              }
+            >
+              <Box>
+                <b>TypeScript based</b> – build type safe applications, all components and hooks
+                export types
+              </Box>
+              <Box>
+                <b>Free and open source</b> – all packages have MIT license, you can use Mantine in
+                any project
+              </Box>
+              <Box>
+                <b>No annoying focus ring</b> – focus ring will appear only when user navigates with
+                keyboard
+              </Box>
+            </Group>
 
-          <Group mt={30}>
-            <Button radius="xl" size="md" className={classes.control}>
-              Get started
-            </Button>
-            <Button variant="default" radius="xl" size="md" className={classes.control}>
-              Source code
-            </Button>
-          </Group>
+            <Group mt={30}>
+              <Button radius="xl" size="md" className={classes.control}>
+                Get started
+              </Button>
+              <Button variant="default" radius="xl" size="md" className={classes.control}>
+                Source code
+              </Button>
+            </Group>
+          </div>
+          <Image src={'/og_image.webp'} className={classes.image} />
         </div>
-        <Image src={'/hero_img.svg'} className={classes.image} />
+      </Container>
+      <Container size="md">
+        <div className={classes.wrapper}>
+          <Grid gutter={80}>
+            <Grid.Col span={{ base: 12, md: 5 }}>
+              <Title className={classes.title} order={2}>
+                A fully featured React components library for your next project
+              </Title>
+              <Text c="dimmed">
+                Build fully functional accessible web applications faster than ever – Mantine includes
+                more than 120 customizable components and hooks to cover you in any situation
+              </Text>
+
+              <Button
+                variant="gradient"
+                gradient={{ deg: 133, from: 'blue', to: 'cyan' }}
+                size="lg"
+                radius="md"
+                mt="xl"
+              >
+                Get started
+              </Button>
+            </Grid.Col>
+            <Grid.Col span={{ base: 12, md: 7 }}>
+              <SimpleGrid cols={{ base: 1, md: 2 }} spacing={30}>
+                {items}
+              </SimpleGrid>
+            </Grid.Col>
+          </Grid>
+        </div>
+      </Container>
+      <Container size={'md'}>
+        <CardsCarousel />
+      </Container>
+    </>
+  );
+}
+
+function CarouselCard({ image, title, category }) {
+  return (
+    <Paper
+      shadow="md"
+      p="xl"
+      radius="md"
+      style={{ backgroundImage: `url(${image})` }}
+      className={classes.card}
+    >
+      <div>
+        <Text className={classes.category} size="xs">
+          {category}
+        </Text>
+        <Title order={3} className={classes.title}>
+          {title}
+        </Title>
       </div>
-    </Container>
+      <Button variant="white" color="dark">
+        Read article
+      </Button>
+    </Paper>
+  );
+}
+
+const data = [
+  {
+    image:
+      'https://images.unsplash.com/photo-1508193638397-1c4234db14d8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80',
+    title: 'Best forests to visit in North America',
+    category: 'nature',
+  },
+  {
+    image:
+      'https://images.unsplash.com/photo-1559494007-9f5847c49d94?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80',
+    title: 'Hawaii beaches review: better than you think',
+    category: 'beach',
+  },
+  {
+    image:
+      'https://images.unsplash.com/photo-1608481337062-4093bf3ed404?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80',
+    title: 'Mountains at night: 12 best locations to enjoy the view',
+    category: 'nature',
+  },
+  {
+    image:
+      'https://images.unsplash.com/photo-1507272931001-fc06c17e4f43?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80',
+    title: 'Aurora in Norway: when to visit for best experience',
+    category: 'nature',
+  },
+  {
+    image:
+      'https://images.unsplash.com/photo-1510798831971-661eb04b3739?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80',
+    title: 'Best places to visit this winter',
+    category: 'tourism',
+  },
+  {
+    image:
+      'https://images.unsplash.com/photo-1582721478779-0ae163c05a60?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80',
+    title: 'Active volcanos reviews: travel at your own risk',
+    category: 'nature',
+  },
+];
+
+function CardsCarousel() {
+  const theme = useMantineTheme();
+  const mobile = useMediaQuery(`(max-width: ${theme.breakpoints.sm})`);
+  const slides = data.map((item) => (
+    <Carousel.Slide key={item.title}>
+      <CarouselCard {...item} />
+    </Carousel.Slide>
+  ));
+
+  return (
+    <Carousel
+      slideSize={{ base: '100%', sm: '50%' }}
+      slideGap={{ base: rem(2), sm: 'xl' }}
+      align="start"
+      slidesToScroll={mobile ? 1 : 2}
+    >
+      {slides}
+    </Carousel>
   );
 }

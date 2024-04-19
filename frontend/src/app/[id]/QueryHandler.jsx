e064fdcf1dbda3a0e9d03e05e7e820aa7e163f30
kartikeygupta1 <kartikraja522@gmail.com>
2024-04-19 12:44:59 +0530

    ..

---
+++ frontend/src/app/[id]/QueryHandler.jsx
@@ -0,0 +1,24 @@
+const { Accordion } = require("@mantine/core");
+
+const QuerryHandler = () => {
+    const {
+        querryList,
+        addnewQuerry,
+        removeQuerry,
+        addParameter,
+        removeQueryParameter
+    } = GraphContext();
+    const fieldNameRef = useRef();
+    const fieldTypeRef = useRef();
+
+    return (
+        <div>
+
+
+            <Accordion defaultActiveKey="0">
+                
+            </Accordion>
+
+
+
+        </div>
